8bc353c8fbc08a0b6448c7576bf3059582c15245
Fabio Rostagno <fadarosta@gmail.com>
2024-12-04 19:14:21 -0300

    condicional completo

--- scripts/pre01.js
+++ scripts/pre01.js
@@ -1,18 +1,8 @@
 let tuNombre = prompt("Para anotarte a la clase escribí tu nombre y apellido")
-let tuPractica = prompt("¿Qué horario preferís?")
+let tuPractica = prompt("¿Qué horario preferís? (Debe ser un número)")
 
-if ((tuNombre === "") && (tuPractica === "")) {
+if ((tuNombre == "") || (tuPractica === "") || isNaN(tuPractica)) {
     alert("Vamos de nuevo, escribí tu nombre completo y elegí el horario para anotarte a la clase.")
 } else {
-    alert("Bienvenido " + tuNombre + ", te anotaste en la clase de Yoga Integral a las " + tuPractica + " horas.")
+    alert("Bienvenido " + tuNombre.trim().toUpperCase() + ", te anotaste en la clase de Yoga Integral a las " + tuPractica + " horas.")
 }
-
-/*
-let cupo = 1
-
-if (cupo == 1) {
-    console.log("Quedan lugares disponibles, ¡anotate!");
-} else {
-    console.log("Ya no hay lugares disponibles, probá con la siguiente clase");
-}
-*/
