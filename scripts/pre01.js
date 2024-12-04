ab9b36823c5268c84882b8f1f358ea50eb372c8f
Fabio Rostagno <fadarosta@gmail.com>
2024-12-03 22:40:46 -0300

    condicional

--- scripts/pre01.js
+++ scripts/pre01.js
@@ -1,1 +1,17 @@
-alert("Hola")
+let tuNombre = prompt("Para anotarte a la clase escribí tu nombre completo")
+
+if (tuNombre == "" ){
+    alert("Vamos de nuevo, escribí tu nombre completo para anotarte a la clase".)
+} else {
+    alert("Bienvenido " + tuNombre + ", te anotaste en la clase de Yoga Integral.")
+}
+
+/*
+let cupo = 1
+
+if (cupo == 1) {
+    console.log("Quedan lugares disponibles, ¡anotate!");
+} else {
+    console.log("Ya no hay lugares disponibles, probá con la siguiente clase");
+}
+*/
