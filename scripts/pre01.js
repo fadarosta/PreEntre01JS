5d084d4fb51569ed904ed34452c2cc51fa27c6f5
Fabio Rostagno <fadarosta@gmail.com>
2024-12-14 20:10:31 -0300

    parseo

--- scripts/pre01.js
+++ scripts/pre01.js
@@ -3,14 +3,14 @@ let inscripciones = 0;
 
 for (let i = 0; i < cupoMaximo; i++) {
     let tuNombre = prompt("👋¡Hola! Para anotarte a la clase, escribí tu nombre y apellido");
-    let tuPractica = prompt("🥰 ¿Qué horario preferís? (Debe ser un número)");
+    let tuPractica = parseInt(prompt("🥰 ¿Qué horario preferís? (Debe ser un número)"));
 
-    if (tuNombre.trim() === "" || tuPractica.trim() === "" || isNaN(tuPractica)) {
+    if (tuNombre.trim() === "" || isNaN(tuPractica)) {
         alert("🤥 Vamos de nuevo. Asegurate de escribir tu nombre completo y que el horario sea un número.");
         i--; // restar uno para repetir el intento
     } else {
         inscripciones++; // sumar un inscripto
-        alert("😍 Bienvenid@ " + tuNombre + ", te anotaste en la clase de Yoga Integral a las " + tuPractica + " horas.");
+        alert("😍 Bienvenid@ " + tuNombre.toUpperCase() + ", te anotaste en la clase de Yoga Integral a las " + tuPractica + " horas.");
         alert("Se ocuparon " + inscripciones + " de " + cupoMaximo + " lugares de la sala");
     }
     // verificador
